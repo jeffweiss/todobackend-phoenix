cb5363b3eeb7c7e9642ac3a091e264262374b143
jeffweiss <jeff.weiss@puppetlabs.com>
2015-05-09 03:28:51 -0700

    update the json render to not nest under data

--- web/views/todo_view.ex
+++ web/views/todo_view.ex
@@ -2,11 +2,11 @@ defmodule Todobackend.TodoView do
   use Todobackend.Web, :view
 
   def render("index.json", %{todos: todos}) do
-    %{data: render_many(todos, "todo.json")}
+    render_many(todos, "todo.json")
   end
 
   def render("show.json", %{todo: todo}) do
-    %{data: render_one(todo, "todo.json")}
+    render_one(todo, "todo.json")
   end
 
   def render("todo.json", %{todo: todo}) do
